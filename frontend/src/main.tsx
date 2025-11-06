4cafbe0db96a1287dd026b0437dd74979d9b29fb
thomas <thomas@tpoe.dev>
2025-11-06 21:53:32 +0100

    fix(frontend): tailwind

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
 import './index.css'
-import 'primereact/resources/themes/lara-light-indigo/theme.css'
-import 'primereact/resources/primereact.min.css'
 import { router } from './router'
 
 createRoot(document.getElementById('root')!).render(
