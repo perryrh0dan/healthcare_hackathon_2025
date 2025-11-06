1f1c82c256fc4ea67d1b90fd3fc9de35be11aef6
thomas <thomas@tpoe.dev>
2025-11-06 22:37:37 +0100

    feat(frontend): radio buttons

--- frontend/src/components/question/question.tsx
+++ frontend/src/components/question/question.tsx
@@ -1,6 +1,8 @@
 import type { QuestionType } from "../../views/DailyQuestions";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group"
 
 type QuestionProps = {
   data: QuestionType, 
@@ -14,9 +16,9 @@ const Question = ({ data, index, total, onNext, onPrev }: QuestionProps) => {
   return (
     <div>
       <div className="rounded-l p-4 flex flex-col gap-2">
-        <h1>Frage {index} von {total}</h1>
+        <h1>Frage {index+1} von {total}</h1>
         <p>{data.question}</p>
-        {body(data.type)}
+        {body(data)}
         <div className="flex flex-row gap-2 w-full">
           <Button className="w-full" variant="outline" onClick={onPrev}>
             Prev
@@ -30,12 +32,21 @@ const Question = ({ data, index, total, onNext, onPrev }: QuestionProps) => {
   );
 }
 
-const body = (type: QuestionProps['data']['type']) => {
-  switch (type) {
+const body = (data: QuestionType) => {
+  switch (data.type) {
     case 'text':
       return <Input />
     case 'enum':
-      return <Input />
+      return (
+         <RadioGroup>
+          {data.options.map((option) => (
+            <div className="flex items-center gap-3">
+              <RadioGroupItem value={option.value.toString()} id={option.value.toString()} />
+              <Label htmlFor="r1">{option.label}</Label>
+            </div>
+          ))}
+         </RadioGroup>
+      )
     case 'scale':
       return <Input />
   } 
